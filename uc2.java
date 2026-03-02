21880f7350266fb737c1b22ecd63d94ff38dcc7d
Rohit Sai Ram <rohitsairam240@gmail.com>
2026-03-02 13:25:26 +0530

    Added OOPSBannerApp banner implementation in UC2

---
+++ uc2.java
@@ -0,0 +1,14 @@
+package oops_banner_app;
+public class uc2 {
+    public static void main(String args[]){
+        
+        System.out.println(" *****    *****   *****    ***** ");
+        System.out.println("*     *  *     *  *    *  *     *");
+        System.out.println("*     *  *     *  *    *  *      ");
+        System.out.println("*     *  *     *  *****    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
+    }
+}
